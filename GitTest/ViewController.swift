030f8976416bead99e00610f8eda37b0ccfceee3
Nathan Bristow (s5109588) <s5109588@mac42944.student.bournemouth.ac.uk>
2019-04-03 11:34:47 +0100

    Revert "Remove Comments"
    This reverts commit 7d01cd491ce6f90d31e9ca496c5feaa43de5031e.

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -1,3 +1,11 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Nathan Bristow (s5109588) on 03/04/2019.
+//  Copyright © 2019 Nathan Bristow (s5109588). All rights reserved.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
