f38df1f5c1c4b3a741c80040e7c4fe3c7a9916eb
Nathan Bristow (s5109588) <s5109588@mac42944.student.bournemouth.ac.uk>
2019-04-03 11:30:33 +0100

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Nathan Bristow (s5109588) on 03/04/2019.
+//  Copyright © 2019 Nathan Bristow (s5109588). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
